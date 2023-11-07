985996556427eae221aa04da52e4dfb926b39e5d
Jake Reed <jreed1997@live.com>
2023-11-06 17:07:18 -0700

    init

---
+++ src/main/java/CrossGenMain.java
@@ -0,0 +1,27 @@
+public class CrossGenMain {
+    //Receive user inputs + hints
+    public static void main(String[] args) {
+        {
+            int wordCount = 0;
+            }
+        }
+}
+    
+    //Elements
+        //FINAL maximum word length
+        //FINAL maximum number of words
+
+    //Main method:
+        //Set word count
+        //Constructs UserBag()
+        //Receiving user inputs/hints
+            //Constructs PuzzleWord() based on inputs
+        //Construct WordList()
+        //Constructs CrossGenFactory()
+
+}
+//Other classes:
+    //UserBag() : Interface methods + Bag Values when generation occurs.
+    //PuzzleWord() : Object class, contains Word, Hint, & Score elements
+    //WordList() Lists PuzzleWords in rank from highest rank to lowest, where rank is
+        //determined by common letters in UserBag
