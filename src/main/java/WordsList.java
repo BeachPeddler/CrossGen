64c2eb10a023a015d6524c99fafa6f6849dd6ac5
kelsiprice <150100432+kelsiprice@users.noreply.github.com>
2023-11-08 18:25:23 -0700

    i made a create priority list method that should sort the wordObjets from puzzleWord in ascending order

--- src/main/java/WordsList.java
+++ src/main/java/WordsList.java
@@ -1,18 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 public class WordsList {
-    private List<String> words;
 
-    public WordsList() {
-        words = new ArrayList<>();
+    private List<PuzzleWord> wordObjects; //
+
+    public WordsList(List<PuzzleWord> wordObjects) {
+
+        this.wordObjects = wordObjects; // this wordObjects is going to be the same as the wordObjects up there
     }
 
-    public void addWord(String word) {
-        words.add(word);
+    public void createPriorityList() {
+        Collections.sort(wordObjects, (a, b) -> Integer.compare(b.wordString(), a.wordHint()));
+    } // compares the word string
+    public PuzzleWord getHighestPriority() {
+        if (wordObjects.isEmpty()) {
+            return null;
+        }
+        return wordObjects.get(0);
     }
 
-    public List<String> getWords() {
-        return words;
+    public PuzzleWord removeHighestPriority() {
+        if (wordObjects.isEmpty()) {
+            return null;
+        }
+        return wordObjects.remove(0);
     }
+
+    public boolean isEmpty() {
+        return wordObjects.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (PuzzleWord word : wordObjects) {
+            result.append(word.toString()).append("\n");
+        }
+        return result.toString();
+    }
+}
 
 }
