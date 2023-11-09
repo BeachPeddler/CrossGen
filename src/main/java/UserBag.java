0e943aaf84ee820f1ba4bc1c285b302dd7468e4d
joel Holguin <joel.efren@gmail.com>
2023-11-08 19:20:23 -0700

    1st commit of UserBag- Joel

--- src/main/java/UserBag.java
+++ src/main/java/UserBag.java
@@ -1,53 +1,135 @@
-import java.util.LinkedList;
+package org.example;
+import java.util.Arrays;
+
+public class UserBag<T> {
+    private T[] wordList;// Wordlist Array
+    public static final int initialCapacity=15;// Array size
+    private int size;
+    private int[] letterFrequency = new int[26];//Alphabet size
+
+    @SuppressWarnings("unchecked")
+//Delete me, just for ref
+    //for ref
 
 
-
-public class UserBag {
-    public static void main(String[] args) {
-        // Create a LinkedList array with some sample strings
-        LinkedList<String> wordList = new LinkedList<>();
-        wordList.add("apple");
-        wordList.add("banana");
-        wordList.add("cherry");
-
-        // Count the frequency of each letter in the LinkedList array
-        countLetterFrequency(wordList);
-        wordScore(wordList);
-
-        // Calculate the number of objects in the LinkedList array
-        int wordCount = calculateWordCount(wordList);
-        System.out.println("Number of objects in the LinkedList array: " + wordCount);
-
+    public UserBag(){// Initialize UserBag
+        wordList = (T[]) new Object[initialCapacity];
+        for (int i = 0; i < initialCapacity; i++) {
+            wordList[i] = null;} //each element initialized to null
+        size=initialCapacity;//size is equal to capacity
     }
 
-    // Method to count the frequency of each letter in the LinkedList array
-    public static void countLetterFrequency(LinkedList<String> wordList) {
-        int[] letterFrequency = new int[26]; // Assuming only lowercase letters
+    public boolean add(T newWord){
+        boolean addSuccess = false;
+        for (int i = 0; i < size; i++) {//iterate through array
+            if (wordList[i] == null) {//if null
+                wordList[i] = newWord;//add new word
+                addSuccess = true;
+                break;
+            } //end if
+        } //end for
+        return addSuccess;
+    } //end add
 
-        for (String word : wordList) {
-            word = word.toLowerCase(); // Convert the string to lowercase for case-insensitive counting
-            for (char i : word.toCharArray()) {
-                if (i >= 'a' && i <= 'z') {
-                    letterFrequency[i - 'a']++;
-                }
+    //return current size
+    public int getCurrentSize() {
+        int currentSize= 0;
+
+        for(int i=0; i<size; i++){
+            if(wordList[i] !=null){
+                currentSize++;
+            }//end if
+        }//end for
+        return currentSize;
+    }//end getCurrentSize
+
+    public boolean remove(T wordToRemove) {//Remove word
+        boolean removed = false; // Flag to track if the word was removed
+        int shiftCount = 0; // Counter for shifting elements
+
+        for (int i = 0; i < size; i++) {
+            if (wordList[i] != null && wordList[i].equals(wordToRemove)) {
+                removed = true; // Word found and removed
+                shiftCount++;
+            } else if (shiftCount > 0) {
+                wordList[i - shiftCount] = wordList[i];
             }
         }
 
-        // Display the letter frequencies
+        if (removed) {
+            for (int i = size - shiftCount; i < size; i++) {
+                wordList[i] = null; // Set the last elements to null
+            }
+            size -= shiftCount; // Update the size
+        }
+
+        return removed;
+
+    }
+    //clear
+    public void clear(UserBag<String> wordBag) {
+        for (int i = 0; i < size; i++) {
+            wordList[i] = null;
+        } //end for
+        size=initialCapacity;
+
+    } //end clear
+    public boolean isEmpty(){
+        return size==0;
+
+    }
+
+    public T get(int index){
+        if (index < 0 || index >=size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }// end if
+        return wordList[index];
+    }
+    @SuppressWarnings("unchecked")
+    public T[] getAllEntries() {
+        T[] entries = Arrays.copyOf(wordList, size);
+        return entries;
+    }
+
+    public int[] countLetterFrequencies() {
+
+        for (int i = 0; i < size; i++) {
+            Object word = wordList[i];
+            if (word instanceof String) {
+                String str = (String) word;
+                for (int j = 0; j < str.length(); j++) {
+                    char c = str.charAt(j);
+                    if (c >= 'a' && c <= 'z') {
+                        letterFrequency[c - 'a']++;
+                    }
+                }
+            }
+        }
         for (int i = 0; i < 26; i++) {
             char letter = (char) ('a' + i);
-            System.out.println(letter + ": " + letterFrequency[i]);
+            int frequency = letterFrequency[i];
+            System.out.println(letter + ": " + frequency);
+        }
+
+        return letterFrequency;
+    }
+
+    public void printWordScores() {
+        for (int i = 0; i < size; i++) {
+            T word = wordList[i];
+            if (word instanceof String) {
+                String str = (String) word;
+                int score = 0;
+
+                for (int j = 0; j < str.length(); j++) {
+                    char c = str.charAt(j);
+                    if (c >= 'a' && c <= 'z') {
+                        score += letterFrequency[c - 'a'];
+                    }
+                }
+
+                System.out.println("Word: " + str + ", Score: " + score);
+            }
         }
     }
-
-    // Method to calculate the number of objects in the LinkedList array
-    public static int calculateWordCount(LinkedList<String> wordList) {
-        return wordList.size();
-    }
-
-    public static void wordScore(LinkedList<String> wordList) {
-        //SOMETHING HERE THAT PRINTS OUT THE SCORE OF EACH WORD
-
-    }
 }
-
