5e37b13802e0747d943271e6440eca9c108e6697
adarcila <arcilasaenzad@gmail.com>
2023-11-15 18:39:52 -0700

    Dependencies

--- src/main/java/PuzzleWord.java
+++ src/main/java/PuzzleWord.java
@@ -3,43 +3,19 @@ import java.util.List;
 import java.util.Arrays;
 
 public class PuzzleWord {
-    private String wordString;
+    private String newWord;
     private String wordHint;
+
     public int wordScore = 0;
 
 
     public PuzzleWord(String word, String hint) {
-        this.wordString = word;
-        this.wordHint = hint;
+        newWord = word;
+        wordHint = hint;
     }
 
     public void setWordScore(int score) {
-        this.wordScore = score;
-    }
-    public int getWordScore() {
-        return this.wordScore;
+        wordScore = score;
     }
 
-    public String getWordString() {
-        return this.wordString;
-    }
-    public void setWordString(String word) {
-        this.wordString = word;
-    }
-
-    public String getWordHint() {
-        return this.wordHint;
-    }
-    public void setWordHint(String hint) {
-        this.wordHint = hint;
-    }
-
-    public char[] toArray() {
-        char[] wordArray = new char[wordString.length()];
-        for (int i = 0; i<wordString.length(); i++) {
-            wordArray[i] = wordString.charAt(i);
-        }
-        return wordArray;
-    }
 }
-
