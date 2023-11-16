fd02705abf9de8747fa80e07505f37a5a73c7ab0
Jacob Reed <jreed1997@live.com>
2023-11-15 19:22:24 -0700

    Revert "Dependencies"
    This reverts commit 5e37b13802e0747d943271e6440eca9c108e6697.

--- src/main/java/PuzzleWord.java
+++ src/main/java/PuzzleWord.java
@@ -3,19 +3,43 @@ import java.util.List;
 import java.util.Arrays;
 
 public class PuzzleWord {
-    private String newWord;
+    private String wordString;
     private String wordHint;
-
     public int wordScore = 0;
 
 
     public PuzzleWord(String word, String hint) {
-        newWord = word;
-        wordHint = hint;
+        this.wordString = word;
+        this.wordHint = hint;
     }
 
     public void setWordScore(int score) {
-        wordScore = score;
+        this.wordScore = score;
+    }
+    public int getWordScore() {
+        return this.wordScore;
     }
 
+    public String getWordString() {
+        return this.wordString;
+    }
+    public void setWordString(String word) {
+        this.wordString = word;
+    }
+
+    public String getWordHint() {
+        return this.wordHint;
+    }
+    public void setWordHint(String hint) {
+        this.wordHint = hint;
+    }
+
+    public char[] toArray() {
+        char[] wordArray = new char[wordString.length()];
+        for (int i = 0; i<wordString.length(); i++) {
+            wordArray[i] = wordString.charAt(i);
+        }
+        return wordArray;
+    }
 }
+
