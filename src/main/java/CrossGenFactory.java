ba456b3677b62eca8238774234106630668b299c
Jacob Reed <jreed1997@live.com>
2023-11-27 19:25:25 -0700

    Completed some methods for CrossGenFactory

--- src/main/java/CrossGenFactory.java
+++ src/main/java/CrossGenFactory.java
@@ -1,10 +1,31 @@
 public class CrossGenFactory {
-
-    public char[][] placeIteratively(UserBag wordsBag) {
+    final static int GRID_DEFAULT = 20; //Sets dimensions of default grid as a square
+    static char empty = '\u0000';
+    public char[][] generate(UserBag wordsBag) {
         PuzzleWord[] wordArray = wordsBag.toArray();
-        String word = wordArray[0].getWordString();
-        int[][] crossword = new int[CrossGenMain.MAX_WORD_COUNT][CrossGenMain.MAX_WORD_COUNT];
-                return null;
+        boolean placementOK;
+        char[][] crossword = placeWord(wordArray[0].getWordString(), new char[GRID_DEFAULT][GRID_DEFAULT], 0, 0, 0);
+        while (!wordsBag.isEmpty()) {
+            for (int i = 1; i < wordArray.length; i++) {
+                String word = wordArray[i].getWordString();
+                char[] charArray = word.toCharArray();
+                for (int j = 0; j < word.length(); j++) {
+                    char currChar = charArray[j];
+                    for (int y = 0; y < crossword.length; y++) {
+                        for (int x = 0; x < crossword[0].length; x++) {
+                            if (crossword[y][x] == currChar) {
+                                placementOK = canPlace(word, crossword, x, y);
+                                if (placementOK) {
+                                    crossword = placeWord(word, crossword, x, y, j);
+                                    wordsBag.remove(wordArray[i]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+                return crossword;
     }
 
     //Creates number of crossword puzzles equal to maximum word count and returns one with highest score.
@@ -26,6 +47,27 @@ public class CrossGenFactory {
         return char[][];
     }
 
+    public char[][] placeWord(String word, char[][] crossword, int x, int y, int junctionIndex) {
+        char junction = crossword[y][x];
+        char[] wordArray = word.toCharArray();
+        boolean downDirection = (crossword[y][x - 1] != empty) && (crossword[y][x + 1] != empty);
+        if (downDirection) {
+            for (int i = 1; i < word.length(); i++) {
+                crossword[y+i-junctionIndex][x] = wordArray[i];
+            }
+        }
+        else {
+            for (int i = 1; i < word.length(); i++) {
+                crossword[y][x+i-junctionIndex] = wordArray[i];
+            }
+        }
+        return crossword;
+    }
+
+    public boolean canPlace(String word, char[][] crossword, int x, int y) {
+        return false;
+    }
+
     //Gives crossword a score based on width:height ratio, # of squares which are filled, and # of squares left blank.
     public static double generateScore(char[][] crossword) {
         double score = 0;
@@ -38,10 +80,9 @@ public class CrossGenFactory {
         if (crossword.length > crossword[0].length) { //Ratio should be <1.
             lengthRatio = ((double) crossword[0].length / crossword.length);
         }
-
         for (int i = 0; i < (crossword.length); i++) {
             for (int j = 0; j < crossword[0].length; j++) {
-                if (crossword[i][j] == ' ') {
+                if (crossword[i][j] == '\u0000') { //should be default char in array
                     squaresEmpty++;
                 }
                 else {
